171d00ccd840b6f4c20f935d8832264baa78cb4f
Christian Zambrana <chrislajas@gmail.com>
2019-07-31 05:15:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,6 +8,12 @@ def roll_call_dwarves# code an argument here
   Sleepy
   Happy
   ]
+  
+  def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end
+end
 end
 
 def summon_captain_planet# code an argument here
