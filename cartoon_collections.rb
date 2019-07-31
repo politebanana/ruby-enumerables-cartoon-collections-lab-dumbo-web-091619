2ac50b9a87b7d7c6e4bd4b4556bfcc97258fd5e1
Christian Zambrana <chrislajas@gmail.com>
2019-07-31 05:20:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,12 @@ end
 
 def summon_captain_planet# code an argument here
   # Your code here
+  planeteer_calls = %w[earth wind fire water heart]
+
+# Expected return value: ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |call| call.capitalize + '!' }
+end
 end
 
 def long_planeteer_calls# code an argument here
